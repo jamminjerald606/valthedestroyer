5f749dc5bdd632ca50c6f624e6ad390fc0ac378b
jamminjerald606 <84991743+jamminjerald606@users.noreply.github.com>
2024-07-18 19:42:05 -0400

    im so sorry to whoever this is

---
+++ cakeatstake.js
@@ -0,0 +1,2 @@
+//credit to jyvee, i used his js script as a ref (hopefully he doesn't mind though, if so, sorry jyvee ;-;)
+document.body.innerHTML = "<style>background-color: black; color:white</style> <h1>Hi there. Friend.</h1> <br> <h1>You don't know me... But I know you.</h1> <h1>I know where you live.</h1> <h1>Don't turn off the computer. Don't leave the website. Stay where you are.</h1>"
